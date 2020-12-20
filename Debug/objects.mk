3946a68cdfd47485d18c0fa3c989edae29611f91
ning <zeus_zhn@netease.com>
2020-12-21 09:20:27 +1000

    beta-5.0, add human detection for video2, , ning, 2020-12-21

--- Debug/objects.mk
+++ Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lpthread -ljpeg -lmi_ipc -lmp4v2 -lrtsio -lcurl -lmbedtls -lmbedcrypto -lmbedx509 -lrtstream -laacenc -lsoxr -lfreetype -lasound -ljson-c -lsodium -lmiss -lzbar
+LIBS := -lpthread -lrtsxmd -ljpeg -lmi_ipc -lmp4v2 -lrtsio -lcurl -lmbedtls -lmbedcrypto -lmbedx509 -lrtstream -laacenc -lsoxr -lfreetype -lasound -ljson-c -lsodium -lmiss -lzbar
 
--- Debug/server/video2/subdir.mk
+++ Debug/server/video2/subdir.mk
@@ -11,6 +11,7 @@ C_SRCS += \
 ../server/video2/jpeg.c \
 ../server/video2/md.c \
 ../server/video2/osd.c \
+../server/video2/spd.c \
 ../server/video2/video2.c \
 ../server/video2/white_balance.c 
 
@@ -22,6 +23,7 @@ OBJS += \
 ./server/video2/jpeg.o \
 ./server/video2/md.o \
 ./server/video2/osd.o \
+./server/video2/spd.o \
 ./server/video2/video2.o \
 ./server/video2/white_balance.o 
 
@@ -33,6 +35,7 @@ C_DEPS += \
 ./server/video2/jpeg.d \
 ./server/video2/md.d \
 ./server/video2/osd.d \
+./server/video2/spd.d \
 ./server/video2/video2.d \
 ./server/video2/white_balance.d 
 
@@ -41,7 +44,7 @@ C_DEPS += \
 server/video2/%.o: ../server/video2/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross GCC Compiler'
-	mips-linux-uclibc-gcc -DVIDEO -DDMALLOC -DAUDIO -I/home/ning/library-mips/cJSON/include -I/home/ning/library-mips/libjpeg/include -I/home/ning/library-mips/jemalloc/include -I/home/ning/library-mips/zbar/include -I/home/ning/library-mips/mp4v2/include -I/home/ning/library-mips/freetype/include -I/home/ning/library-mips/mi/include -I/home/ning/library-mips/realtek/include -I/home/ning/library-mips/json-c/include -O0 -g3 -Wall -c -Wl,-Map=gcc.map -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	mips-linux-uclibc-gcc -DVIDEO -DDMALLOC -DAUDIO -I/home/ning/library-mips/cJSON/include -I/home/ning/library-mips/rtsxmd/include -I/home/ning/library-mips/libjpeg/include -I/home/ning/library-mips/jemalloc/include -I/home/ning/library-mips/zbar/include -I/home/ning/library-mips/mp4v2/include -I/home/ning/library-mips/freetype/include -I/home/ning/library-mips/mi/include -I/home/ning/library-mips/realtek/include -I/home/ning/library-mips/json-c/include -O0 -g3 -Wall -c -Wl,-Map=gcc.map -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
